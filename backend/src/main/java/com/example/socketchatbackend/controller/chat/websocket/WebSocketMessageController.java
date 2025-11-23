cdc4e686f3ebbd9087492a43df64eb0d5986b968
partmant <ghdckdgml191@gmail.com>
2025-11-24 03:22:42 +0900

    [BE] refactor(WebSocketMessage): 채팅 메시징 구조를 WebSocket TALK 전용으로 리팩토링 및 통합
    - HTTP 기반의 RoomMessageController를 삭제하고,
    실시간 채팅 메시지 전송 로직을 WebSocket으로 통합

--- backend/src/main/java/com/example/socketchatbackend/controller/chat/websocket/WebSocketMessageController.java
+++ backend/src/main/java/com/example/socketchatbackend/controller/chat/websocket/WebSocketMessageController.java
@@ -1,22 +1,27 @@
 package com.example.socketchatbackend.controller.chat.websocket;
 
+import com.example.socketchatbackend.dto.chat.message.MessageType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 import com.example.socketchatbackend.dto.chat.message.RoomMessageRequest;
-import com.example.socketchatbackend.service.chat.message.MessageService;
+import com.example.socketchatbackend.service.chat.room.RoomMessageService;
 
 @Controller
 public class WebSocketMessageController {
 
-    private final MessageService messageService;
+    private final RoomMessageService roomMessageService;
 
-    public WebSocketMessageController(MessageService messageService) {
-        this.messageService = messageService;
+    public WebSocketMessageController(RoomMessageService roomMessageService) {
+        this.roomMessageService = roomMessageService;
     }
 
     @MessageMapping("/chat.send")
     public void send(RoomMessageRequest request) {
-        messageService.broadcast(request);
+        if (request.type() != MessageType.TALK) {   // TALK 타입만 처리
+            return;
+        }
+
+        roomMessageService.sendTalkMessage(request);
     }
 }
